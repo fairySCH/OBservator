5390a862cfbbd1248d1e96f6db12e133fd5bd8bf
sholee-pt <sholeept@dgu.ac.kr>
2024-11-26 01:00:54 +0900

    Feat: Add TCP connect configuration

--- Backend/observator/src/main/java/com/happy/observator/ObservatorApplication.java
+++ Backend/observator/src/main/java/com/happy/observator/ObservatorApplication.java
@@ -5,21 +5,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.happy.observator.service.UserService;
+import com.happy.observator.server.HighSpeedTcpServer;
 
 @SpringBootApplication
 public class ObservatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ObservatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ObservatorApplication.class, args);
+    }
 
-    // 서버-Mysql 연결시 이미 등록된 계정이므로 주석 처리
-    // 사용 저장소에 따라 application.properties의 저장소 주석 변경
-	@Bean
-    CommandLineRunner run(UserService userService) {
+    // // 서버-Mysql 연결시 이미 등록된 계정이므로 주석 처리
+    // // 사용 저장소에 따라 application.properties의 저장소 주석 변경
+	// @Bean
+    // CommandLineRunner run(UserService userService) {
+    //     return args -> {
+    //         userService.saveUser("user", "password");
+    //     };
+    // }
+
+    @Bean
+    CommandLineRunner startTcpServer(HighSpeedTcpServer tcpServer) {
         return args -> {
-            userService.saveUser("user", "password");
+            //userService.saveUser("user", "password");
         };
     }
 }
