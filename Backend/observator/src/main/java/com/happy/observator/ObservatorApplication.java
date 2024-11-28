2fec2702ee66b6ef2d30e77d7274cc74b60a8a40
sholee-pt <sholeept@dgu.ac.kr>
2024-11-28 17:00:44 +0900

    Add: Use RestTemplate for TCP Connection

--- Backend/observator/src/main/java/com/happy/observator/ObservatorApplication.java
+++ Backend/observator/src/main/java/com/happy/observator/ObservatorApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import com.happy.observator.server.HighSpeedTcpServer;
 import com.happy.observator.service.UserService;
@@ -16,14 +17,19 @@ public class ObservatorApplication {
         SpringApplication.run(ObservatorApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     // 서버-Mysql 연결시 이미 등록된 계정이므로 주석 처리
     // 사용 저장소에 따라 application.properties의 저장소 주석 변경
     @Bean
     CommandLineRunner run(UserService userService, TradingOrderProcessor tradingOrderProcessor) {
         return args -> {
             // userService.saveUser("user", "password");
-            String jsonOrder = "{\"userId\":1,\"action\":\"buy\",\"amount\":\"5000\",\"targetTime\":\"15:30:00.500\"}";
-            tradingOrderProcessor.processReceivedOrder(jsonOrder);
+            // String jsonOrder = "{\"userId\":1,\"action\":\"buy\",\"amount\":\"5000\",\"targetTime\":\"15:30:00.500\"}";
+            // tradingOrderProcessor.processReceivedOrder(jsonOrder);
         };
     }
 
